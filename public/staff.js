dc21582458adb4f28c93edf296d742d2d55f2a6b
Nonthpawit S. <89679927+MonkeyZ-04@users.noreply.github.com>
2026-01-14 00:02:48 +0700

    Update

--- public/staff.js
+++ public/staff.js
@@ -89,6 +89,7 @@ const renderTimeslotDashboard = () => {
 
     const generatedTimes = generateTimeSlots('16:30', '20:30', 30);
     const allTimeSet = new Set([...dataTimes, ...generatedTimes]);
+    const excludedTimes = ['18:50', '19:10', '19:30', '19:50', '20:10', '20:30'];
     const sortedTimes = Array.from(allTimeSet)
                       .sort((a,b) => a.localeCompare(b))
                       .filter(time => !excludedTimes.includes(time));
